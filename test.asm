8acbdf63f513cdb7f4fd40672e39214d11f813c4
Javier M. Raut <javier.raut@gmail.com>
2024-03-02 00:22:06 +0800

    Adedd basic Hello World!

--- test.asm
+++ test.asm
@@ -1,1 +1,21 @@
+.data
+    helloWorld: .asciiz "Hello World!"
+    boilerplate: .asciiz "I am "
+    name: .asciiz "Javier M. Raut"
+    space: .asciiz "\n"
+.text
+    li $v0, 4
+    la $a0, helloWorld
+    syscall
 
+    li $v0, 4
+    la $a0, space
+    syscall
+    
+    li $v0, 4
+    la $a0, boilerplate
+    syscall
+
+    li $v0, 4
+    la $a0, name
+    syscall
